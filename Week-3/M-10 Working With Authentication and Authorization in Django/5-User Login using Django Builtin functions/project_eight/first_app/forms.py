553231f44a86b8b51d17f6ada8c4e9cfc65a2e19
jafrul <jafrulalamtusher22@gmail.com>
2023-06-13 07:39:35 +0600

    new add

--- Week-3/M-10 Working With Authentication and Authorization in Django/5-User Login using Django Builtin functions/project_eight/first_app/forms.py
+++ Week-3/M-10 Working With Authentication and Authorization in Django/5-User Login using Django Builtin functions/project_eight/first_app/forms.py
@@ -10,7 +10,7 @@ class RegisterForm(UserCreationForm):
     last_name = forms.CharField(
         widget=forms.TextInput(attrs={'id': 'required'}))
     email = forms.CharField(
-        widget=forms.TextInput(attrs={'id': 'required'}))
+        widget=forms.EmailInput(attrs={'id': 'required'}))
 
     class Meta:
         model = User
